3ae3ee97d04a5f42e4b8a1cefdc51e232a3ae341
kamva-hanisi <lucashanisi@gmail.com>
2023-06-27 11:08:18 -0700

    function testing again

--- func_caller.c
+++ func_caller.c
@@ -8,7 +8,6 @@
  */
 int func_caller(char formatter, va_list *args)
 {
-
 	print_func print_options[] = {
 		{'c', print_char},
 		{'s', print_string},
@@ -17,21 +16,23 @@ int func_caller(char formatter, va_list *args)
 		{'\0', NULL}};
 
 	int print_options_size = sizeof(print_options) / sizeof(print_options[0]);
+	int i, characters_number = 0;
 
-	int i;
-	int count = 0;
-	int characters_number = 0;
+	if (formatter == '%')
+	{
+		_putchar(37);
+		return (1);
+	}
 
 	for (i = 0; i < print_options_size; i++)
 	{
 		if (print_options[i].formatter == formatter)
 		{
 			characters_number += print_options[i].function_writer(args);
-			count++;
 		}
 	}
 
-	if (count == 0)
+	if (characters_number == 0 && formatter)
 	{
 		_putchar(formatter);
 		characters_number++;
